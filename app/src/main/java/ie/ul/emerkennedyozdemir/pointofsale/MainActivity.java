81a0a36d6196d01ad438d95ca2ca49db0b23c7b1
EmerKO <43354325+EmerKO@users.noreply.github.com>
2018-10-16 22:05:35 +0100

    basic menu

--- app/src/main/java/ie/ul/emerkennedyozdemir/pointofsale/MainActivity.java
+++ app/src/main/java/ie/ul/emerkennedyozdemir/pointofsale/MainActivity.java
@@ -1,6 +1,8 @@
 package ie.ul.emerkennedyozdemir.pointofsale;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +10,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mNameTextView, mQuantityTextView, mDateTextView;
+    private Item mCurrentItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameTextView = findViewById(R.id.name_text);
+        mQuantityTextView = findViewById(R.id.quantity_text);
+        mDateTextView = findViewById(R.id.date_text);
+
+
+        //Boilerplate code. Dom't mess with it.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,15 +36,24 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               //for now just practise showing anitem on the screen
+               mCurrentItem = Item.getDefaultItem();
+               showCurrentItem();
+
             }
         });
     }
 
+    private void showCurrentItem() {
+        mNameTextView.setText(mCurrentItem.getName());
+        mQuantityTextView.setText(getString(R.string.quantity_format, mCurrentItem.getQuantity()));
+        mDateTextView.setText(getString(R.string.date_format, mCurrentItem.getDeliveryDateString()));
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present. Boilerplate code
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -40,9 +63,19 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.action_reset:
+                mCurrentItem = new Item();
+                showCurrentItem();
+                return true;
+            case R.id.action_settings:
+                //startActivity(new Intent(Settings.ACTION_SETTINGS));
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
 
-        //noinspection SimplifiableIfStatement
+                return true;
+        }
+
+        int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
